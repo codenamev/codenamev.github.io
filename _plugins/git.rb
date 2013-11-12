f590fbacc87c386a725cef6fdd84fee39977e71d
Valentino <valentino@reenhanced.com>
2013-11-12 14:15:31 -0500

    Cleanup starred repo styles

--- _plugins/git.rb
+++ _plugins/git.rb
@@ -23,8 +23,8 @@ module Jekyll
               <h4>#{ starred_repo.name }</h4>
               <small>#{ starred_repo.description }</small>
               <figcaption>
-                <h3><img src="http://www.gravatar.com/avatar/#{ starred_repo.owner.gravatar_id }" width="24" height="24" algin="left">#{ starred_repo.name }</h3>
-                <span><a href="https://github.com/#{ starred_repo.owner.login }" target="_blank">#{ starred_repo.owner.login }</a></span>
+                <h3>#{ starred_repo.name }</h3>
+                <span><a href="https://github.com/#{ starred_repo.owner.login }" target="_blank"><img src="http://www.gravatar.com/avatar/#{ starred_repo.owner.gravatar_id }" width="24" height="24" algin="left">#{ starred_repo.owner.login }</a></span>
                 <a href="#{ repo_url }" target="_blank">Check it out</a>
               </figcaption>
             </figure>
